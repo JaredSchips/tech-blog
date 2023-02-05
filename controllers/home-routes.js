b51f9ea8e6aa5555c015b3c94550cb8cbfca0156
JaredSchips <jared.schips@gmail.com>
2023-02-05 16:28:38 -0500

    boilerplate hbs website with login functionality

---
+++ controllers/home-routes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router()
+
+// Homepage
+router.get('/', async (req, res) => {
+  try {
+    res.render('homepage', {
+      loggedIn: req.session.loggedIn
+    })
+  }
+  catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+})
+
+// Login
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/')
+    return
+  }
+  res.render('login', {
+    loggedIn: req.session.loggedIn
+  })
+})
+
+module.exports = router
